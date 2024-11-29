a2fa6a3e4c7e05a68cea6a679e9ae4d0d4e4771c
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2024-11-29 13:27:32 +0000

    refactor CardNumber

--- src/components/CardNumber/CardNumber.jsx
+++ src/components/CardNumber/CardNumber.jsx
@@ -1,40 +1,26 @@
 import  { useState } from 'react';
-
-import { TiPlus, TiMinus } from "react-icons/ti";
-import styles from './CardNumber.module.css'; // Стилі для компонента
+import { Input, Button } from 'antd';
+import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai';
+import styles from './CardNumber.module.css';
 const CardNumber = ({ title }) => {
-  const [value, setValue] = useState(20); // Початкове значення
+  const [value, setValue] = useState(20);
   const handleIncrease = () => {
-    if (value < 200) {
-      setValue(value + 1);
-    }
+    if (value < 200) setValue(value + 1);
   };
   const handleDecrease = () => {
-    if (value > 1) {
-      setValue(value - 1);
-    }
+    if (value > 1) setValue(value - 1);
   };
   return (
     <div className={styles.container}>
-      <div className={styles.labelContainer}>
-        <span className={styles.title}>{title}</span>
+      <div className={styles.wrap}>
+      <span className={styles.title}>{title}</span>
 
-        <div className={styles.inputContainer}>
-          <button onClick={handleDecrease} className={styles.button}>
-            <TiMinus className={styles.inputIcon}/>
-          </button>
-          <input
-            type="number"
-            value={value}
-            readOnly
-            className={styles.input}
-            style={{ appearance: 'none' }} 
-          />
-          <button onClick={handleIncrease} className={styles.button}>
-            <TiPlus className={styles.inputIcon}/>
-          </button>
-        </div>
+      <div  className={styles.box}>
+        <Button onClick={handleDecrease} icon={<AiOutlineMinus />} className={styles.buttonMinus}/>
+        <Input value={value} readOnly style={{ width: '305px', textAlign: 'center' }} className={`${styles.input} ${styles.inputText}`}/>
+        <Button onClick={handleIncrease} icon={<AiOutlinePlus />} className={styles.buttonPlus}/>
       </div>
+</div>
     </div>
   );
 };
