18e256e36cd870c07b7e2c5ae2e0786ef7885f77
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2024-11-30 16:18:03 +0000

    CardTime updated

--- src/components/CardForm/CardForm.jsx
+++ src/components/CardForm/CardForm.jsx
@@ -3,24 +3,24 @@ import styles from './CardForm.module.css';
 import CardType from '../../components/CardType/CardType.jsx';
 import CardTime from '../../components/CardTime/CardTime.jsx';
 import CardNumber from '../../components/CardNumber/CardNumber.jsx';
-import CardLevel from '../../components/CardLevel/CardLevel.jsx'; 
+import CardLevel from '../../components/CardLevel/CardLevel.jsx';
 import Button from '../../components/Button/Button.jsx';
 import typeOptions from '../../data/typeOptions.js';
-import difficultiOptions from '../../data/difficultiOptions.js';
+import difficultiOptions from '../../data/difficultyOptions.js'; // Зверніть увагу на правильне написання
 import timeOptions from '../../data/timeOptions.js';
 const CardForm = () => {
   return (
     <Formik
       initialValues={{
         trainingType: "запам'ятовування чисел",
-        difficulty: 'без розділу',
+        difficulty: 'без розряду',
         rememberTime: '5 хв',
         recallTime: '5 хв',
         rowsCount: 20,
-        evaluationLevel: 'Учень', 
+        evaluationLevel: 'Учень',
       }}
       onSubmit={(values) => {
-        console.log('Submitted values:', values); 
+        console.log('Submitted values:', values);
       }}
     >
       {() => (
@@ -32,27 +32,36 @@ const CardForm = () => {
           />
           <CardLevel
             label="Рівень оцінювання:"
-            name="evaluationLevel" 
+            name="evaluationLevel"
           />
-
           <CardType
             label="Розрядність:"
             name="difficulty"
-            options={difficultiOptions}
+            options={difficultiOptions} 
           />
           <CardNumber title="Кількість рядків:" name="rowsCount" />
 
+
           <div className={styles.timeOptionsBlock}>
-            <CardTime
-              label="Запам'ятати за:"
-              name="rememberTime"
-              options={timeOptions}
-            />
-            <CardTime
-              label="Згадати за:"
-              name="recallTime"
-              options={timeOptions}
-            />
+            <label htmlFor="rememberTime" 
+            className={styles.timeRemember}>
+              Запам´ятати за:</label>
+
+            <div className={styles.timeOptionsWrap}>
+              <CardTime
+                name="rememberTime"
+                options={timeOptions}
+              />
+                          <label htmlFor="recallTime" 
+            className={styles.timeRecall}>
+              Згадати за:</label>
+              <CardTime
+               
+                name="recallTime"
+                options={timeOptions}
+              />
+            </div>
+
           </div>
           <div className={styles.btnBlock}>
             <Button type="submit" label="Старт!" />
