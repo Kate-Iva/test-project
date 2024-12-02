b02aceda31e1ff53b5b9ba5003b914f6f309a2c4
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2024-12-02 21:22:10 +0000

    Calendar component created

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,12 @@
+import styles from './Calendar.module.css'; 
+
+
+const Calendar = () => {
+
+    return (
+      <div className={styles.container}>
+<p>Here will be calendar</p>
+      </div>
+    );
+  };
+  export default Calendar;
--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -1,11 +1,12 @@
 import styles from './CalendarPage.module.css'; 
-
+import Calendar from "../../components/Calendar/Calendar.jsx";
 
 const CalendarPage = () => {
 
     return (
       <div className={styles.container}>
 <h1>Calendar</h1>
+<Calendar className={styles.calendar}/>
       </div>
     );
   };
